5b1bb0fe6b86389a6f60f12ad03fbd4422873fcb
josh <chencl227@gmail.com>
2017-05-17 21:10:40 +0700

    添加轮播图

--- src/data/carouseljson.tsx
+++ src/data/carouseljson.tsx
@@ -3,19 +3,11 @@
 const CarouselJson = [{
     id: 1,
     alt: '图片描述1',
-    url: 'https://zos.alipayobjects.com/rmsportal/hzPBTkqtFpLlWCi.jpg'
+    url: 'img/img1.jpeg'
 }, {
     id: 2,
     alt: '图片描述2',
     url: 'https://zos.alipayobjects.com/rmsportal/gGlUMYGEIvjDOOw.jpg'
-}, {
-    id: 3,
-    alt: '图片描述3',
-    url: 'https://zos.alipayobjects.com/rmsportal/hzPBTkqtFpLlWCi.jpg'
-}, {
-    id: 4,
-    alt: '图片描述4',
-    url: 'https://zos.alipayobjects.com/rmsportal/hzPBTkqtFpLlWCi.jpg'
 }];
 
 export default CarouselJson;
